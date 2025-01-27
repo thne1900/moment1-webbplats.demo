baa1488918ad9f69aeb8bf586b361df6cee680d7
Therese Nevander <thne1900@student.miun.se>
2025-01-27 16:31:06 +0100

    Bytte gren, körde kommando och sparade de senaste ändringarna.

---
+++ dist/index.65381db2.js
@@ -0,0 +1,2 @@
+document.getElementById("menu-hamburger").addEventListener("click",function(){document.getElementById("links").classList.toggle("active")});
+//# sourceMappingURL=index.65381db2.js.map
